88f700c48201864a4388e55627779c8013dd8930
caadiq <caadiq@gmail.com>
2024-04-25 17:02:04 +0900

    HomeScreen에 CenterAlignedTopAppBar 추가

--- app/src/main/java/com/beemer/unofficial/fromis_9/view/screen/HomeScreen.kt
+++ app/src/main/java/com/beemer/unofficial/fromis_9/view/screen/HomeScreen.kt
@@ -1,9 +1,38 @@
 package com.beemer.unofficial.fromis_9.view.screen
 
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
+import com.beemer.unofficial.fromis_9.R
+import com.beemer.unofficial.fromis_9.ui.theme.DarkGray
+import com.beemer.unofficial.fromis_9.ui.theme.NanumSquareRoundExtraBold
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenHome() {
-    Text(text = "Home")
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "fromis_9",
+                color = DarkGray,
+                fontSize = 26.sp,
+                fontFamily = NanumSquareRoundExtraBold
+            )
+        },
+        actions = {
+            IconButton(onClick = {
+
+            }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.icon_clover),
+                    contentDescription = "앱 정보"
+                )
+            }
+        }
+    )
 }
