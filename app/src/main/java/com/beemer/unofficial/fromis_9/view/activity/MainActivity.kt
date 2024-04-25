c0910e78b39703ba800f3356fc846d12dd83727a
caadiq <caadiq@gmail.com>
2024-04-25 23:52:00 +0900

    뒤로 가기 두 번 눌러서 앱 종료 추가

--- app/src/main/java/com/beemer/unofficial/fromis_9/view/activity/MainActivity.kt
+++ app/src/main/java/com/beemer/unofficial/fromis_9/view/activity/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.beemer.unofficial.fromis_9.view.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import com.beemer.unofficial.fromis_9.ui.theme.Fromis_9Theme
 import com.beemer.unofficial.fromis_9.view.screen.MainScreen
 
@@ -18,9 +26,27 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
+                    OnBackPressed()
                     MainScreen()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun OnBackPressed() {
+    val context = LocalContext.current
+    var backPressedState by remember { mutableStateOf(true) }
+    var backPressedTime: Long = 0
+
+    BackHandler(enabled = backPressedState) {
+        if (System.currentTimeMillis() - backPressedTime <= 2000) {
+            (context as MainActivity).finish()
+        } else {
+            backPressedState = true
+            Toast.makeText(context, "뒤로 가기 버튼을 한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+        }
+        backPressedTime = System.currentTimeMillis()
     }
 }
